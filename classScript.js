3f96b8ffb4f68a77ca6f0e905a0e741b9c2e3538
Gabriel Sgarlato <ga.sgarlato.10@gmail.com>
2025-10-30 09:20:05 -0400

    changed

--- classScript.js
+++ classScript.js
@@ -1,12 +1,10 @@
-document.getElementById("changeP").innerHTML="1";
+document.getElementById("changeP").innerHTML="3";
 document.getElementById("addButton").innerHTML="add value";
 
 //I am selecting the button and adding an event listener for a mouse click that will
 //run a function
 document.getElementsById("addButton").addEventListener("click" , function () {
     let origin=document.getElementById("changeP").innerHTML;
-    console.log(origin);
-    //cating to covert string to number
     origin=parseInt(origin)+1;
     document.getElementById("changeP").innerHTML=origin
 });
