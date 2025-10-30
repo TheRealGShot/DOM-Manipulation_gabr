288b2cd859df0fa55b0113e0995641a4a2917500
Gabriel Sgarlato <ga.sgarlato.10@gmail.com>
2025-10-30 09:22:59 -0400

    added the add button functionality

--- classScript.js
+++ classScript.js
@@ -1,13 +1,8 @@
 document.getElementById("changeP").innerHTML="3";
 document.getElementById("addButton").innerHTML="add x";
 
-
-document.getElementsById("addButton").addEventListener("click" , function () {
-    let origin=document.getElementById("changeP").innerHTML;
-    origin=parseInt(origin)+1;
-    document.getElementById("changeP").innerHTML=origin
+document.getElementById("addButton").addEventListener("click",function(){
+    let a=parseInt(document.getElementsById("changeP").innerHTML);
+    a=a+1;
+    document.getElementsById("changeP").innerHTML=a;
 });
-
-
-
-document.getElementsByClassName("card")[1].style.backgroundColor="blue";
